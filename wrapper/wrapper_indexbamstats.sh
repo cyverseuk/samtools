d53a7ca53dd0abc48a3cb1cf88532d43a5dec56a
aliceminotto <i.m.e@hotmail.it>
2017-05-19 16:15:37 +0100

    added app to index bam and get stats, with run.json, wrapper and template tests

---
+++ wrapper/wrapper_indexbamstats.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function debug {
+  echo "creating debugging directory"
+mkdir .debug
+for word in ${rmthis}
+  do
+    if [[ "${word}" == *.sh ]] || [[ "${word}" == lib ]]
+      then
+        mv "${word}" .debug;
+      fi
+  done
+}
+
+rmthis=`ls`
+echo ${rmthis}
+
+INPUTSU="${bam_file}"
+echo "Input file is " "${INPUTSU}"
+
+OUTPUT=$(basename ${INPUTSU})
+OUTPUT=${OUTPUT%%.*}
+CMDLINEARG="samtools index ${INPUTSU}; samtools idxstats ${INPUTSU} >> stats.txt"
+
+echo ${CMDLINEARG};
+
+chmod +x launch.sh
+
+echo  universe                = docker >> lib/condorSubmitEdit.htc
+echo docker_image            =  cyverseuk/samtools:v0.1.19 >> lib/condorSubmitEdit.htc ######
+echo executable               =  ./launch.sh >> lib/condorSubmitEdit.htc #####
+echo arguments                          = ${CMDLINEARG} >> lib/condorSubmitEdit.htc
+echo transfer_input_files = ${INPUTSU}, launch.sh >> lib/condorSubmitEdit.htc
+#echo transfer_output_files = ${OUTPUTSU} >> lib/condorSubmitEdit.htc
+cat /mnt/data/apps/index_bam_and_stats/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
+
+less lib/condorSubmitEdit.htc
+
+jobid=`condor_submit -batch-name ${PWD##*/} lib/condorSubmitEdit.htc`
+jobid=`echo $jobid | sed -e 's/Sub.*uster //'`
+jobid=`echo $jobid | sed -e 's/\.//'`
+
+#echo $jobid
+
+#echo going to monitor job $jobid
+condor_tail -f $jobid
+
+debug
+
+exit 0
